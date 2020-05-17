d3d7cf1dda64b61043178dbe4442a7a656fc1825
Krendzel <krendzel@gmail.com>
2020-05-17 20:14:05 +0200

    Add prop types

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import styles from './Hero.scss';
+import React from "react";
+import styles from "./Hero.scss";
 
-class Hero extends React.Component {
-    render() {
-        return(
-            <header className={styles.component}>
-                <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
-                <h2 className={styles.title}>Things to do</h2>
-            </header>
-        )
-    }
-}
+// class Hero extends React.Component {
+//   render() {
+//     return (
+//       <header className={styles.component}>
+//         <img
+//           className={styles.image}
+//           src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
+//         />
+//         <h2 className={styles.title}>Things to do</h2>
+//       </header>
+//     );
+//   }
+// }
+
+ const Hero = props => (
+  <header className={styles.component}>
+    <img
+      className={styles.image}
+      src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
+    />
+    <h2 className={styles.title}>{props.titleText}</h2>
+  </header>
+);
 
 export default Hero;
